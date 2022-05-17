eaf2ce8c04efb1b8e05d14064745ec4e971689ce
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2022-05-17 23:23:01 +0100

    working app - setup controllers and views for home dictionary and game

---
+++ app/controllers/dictionary_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class DictionaryController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def show; end
+
+  def edit; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'game/index'
+  get 'game/new'
+  get 'game/show'
+  get 'game/edit'
+
+  get 'dictionary/index'
+  get 'dictionary/new'
+  get 'dictionary/show'
+  get 'dictionary/edit'
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
